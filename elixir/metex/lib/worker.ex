0ba9dfb6ace66ff79fa4ad4a0b00740ddd4a46c5
silmood <dsilmood@gmail.com>
2021-01-29 18:08:41 -0700

    🚀 Create metex project

---
+++ elixir/metex/lib/worker.ex
@@ -0,0 +1,47 @@
+defmodule Metex.Worker do
+
+  @api_key "api_key"
+
+  def temperature_of(location) do
+    result = url_of(location)
+      |> HTTPoison.get
+      |> parse_response
+    
+    case result do
+      {:ok, temp} ->
+        "#{location}: #{temp}°C"
+      :error ->
+        "#{location} not found"
+    end
+  end
+
+  defp url_of(location) do
+    location = URI.encode(location)
+    "http://api.openweathermap.org/data/2.5/weather?q+#{location}&appid=#{@api_key}"
+  end
+
+  defp parse_response({:ok, %HTTPoison.Response{body: body, status_code: 200}}) do
+    body
+    |> Jason.decode!
+    |> compute_temperature
+  end
+
+  defp parse_response(_) do
+    :error
+  end
+
+  defp compute_temperature(json) do
+    try do
+      (json["main"]["temp"] - 273.15)
+      |> Float.round(1)
+      |> (&({:ok, &1})).()
+    rescue
+      _ -> :error
+    end
+  end
+
+  defp map(arg, foo) do
+    foo.(arg) 
+  end
+
+end
